7d92fbf4fc0ec2b9506a51716b937db9aa1ac28a
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-11 01:14:08 +0530

    [Prasoon] Largest among three numbers

---
+++ Largest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class Largest
+    {
+        public void LargestAmongThree()
+        {
+            Console.WriteLine("Enter the first number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            int o = Convert.ToInt32(Console.ReadLine());
+            if(n>m && n>o)
+            {
+                Console.WriteLine("n is the the largest"+n);
+            }
+            else if(m>n && m>o)
+            {
+                Console.WriteLine("m is the largest" + m);
+            }
+            else
+            {
+                Console.WriteLine("o is the largest" + o);
+            }
+        }
+    }
+}
