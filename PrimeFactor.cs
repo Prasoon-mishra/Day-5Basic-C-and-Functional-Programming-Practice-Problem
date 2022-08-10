4e9cbde8db8256a2c6e4a607c759bccc6e4416be
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-11 01:02:26 +0530

    [Prasoon] Prime factors

---
+++ PrimeFactor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class PrimeFactor
+    {
+        public void Factors()
+        {
+            int c = 2;
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n > 1)
+            {
+                if (n % c == 0)
+                {
+                    Console.Write(c + " ");
+                    n /= c;
+                }
+                else
+                    c++;
+            }
+        }
+    }
+}
