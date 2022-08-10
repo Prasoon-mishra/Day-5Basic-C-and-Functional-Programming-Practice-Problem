e423c6b8899d339874dd1bc706a632b0d447b0a8
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-11 00:37:36 +0530

    [prasoon] Heads and Tail Percentage

---
+++ HeadorTails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class HeadorTails
+    {
+        public void HeadsOrTailPercentage()
+        {
+            Random random = new Random();
+            Console.WriteLine("Enter the number of terms");
+            int n=Convert.ToInt32(Console.ReadLine());
+            double k = n;
+            double head = 0D, tail = 0D;
+            while (n>0)
+            {
+                int checkValue = random.Next(0, 2);
+                
+                if (checkValue == 1)
+                {
+                    Console.WriteLine("Heads");
+                    head++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    tail++;
+                }
+                n--;
+
+            }
+            Console.WriteLine("Heads comes :"+head);
+            Console.WriteLine("Tail comes :" + tail);
+            Console.WriteLine("Heads Percentage : " + ((head / k) * 100));
+            Console.WriteLine("Tailss Percentage : " + ((tail / k) * 100));
+        }
+    }
+}
