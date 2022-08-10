1f1801fc38a48eedad2543062494f60afd749dd5
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-11 00:59:20 +0530

    [Prasoon] Harmonic Series Program

---
+++ HarmonicNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class HarmonicNumber
+    {
+        public void Harmonic()
+        {
+            Console.WriteLine("Harmonic number series program");
+            Console.WriteLine("Enter the number of terms: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            double i = 1;
+            double sum = 0;
+            while (i<=n)
+            {
+                sum = sum + (1 / i);
+                i++;
+            }
+            Console.WriteLine("The sum of " + i + "th term is=" + sum); 
+        }
+    }
+}
